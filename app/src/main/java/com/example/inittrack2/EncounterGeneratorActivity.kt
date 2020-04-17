5450ebdb007d5aeaaea9d501793907982ed5f2db
Fivos Iliadis <eternallistener@gmail.com>
2020-04-17 15:11:06 +0300

    Show initial grid-map with correct spacing

--- app/src/main/java/com/example/inittrack2/EncounterGeneratorActivity.kt
+++ app/src/main/java/com/example/inittrack2/EncounterGeneratorActivity.kt
@@ -2,6 +2,7 @@ package com.example.inittrack2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,19 +14,31 @@ class EncounterGeneratorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grid_test)
         tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Rogue", 5)));
         tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Monster", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Paladin", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Monster", 5)));
         tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Wizard", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Druid", 5)));
+
+        tiles.add(Tile(Character("Fivos", 2, "Paladin", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Paladin", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Monster", 5)));
         tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
-        tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
-        tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
-        tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
-        tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
-        tiles.add(Tile(Character("Fivos", 2, "Cleric", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Sorcerer", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Paladin", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Monster", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Ranger", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Wizard", 5)));
+        tiles.add(Tile(Character("Fivos", 2, "Paladin", 5)));
+
 
         val mRecyclerView : RecyclerView = findViewById(R.id.recyclerview_id)
 //        mRecyclerView.setHasFixedSize(true)
         val mAdapter = GridAdapter(tiles, this)
-        val mLayoutManager = LinearLayoutManager(this)
+        val mLayoutManager = GridLayoutManager(this, 5)
 
         mRecyclerView.layoutManager = mLayoutManager
         mRecyclerView.adapter = mAdapter
