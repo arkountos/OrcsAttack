73f6154ed4c24a500d399e7fd0537ae2dec20176
arkountos <eternallistener@gmail.com>
2020-04-28 14:37:59 +0300

    Stream progress

--- app/src/main/java/com/example/inittrack2/EncounterGeneratorActivity.kt
+++ app/src/main/java/com/example/inittrack2/EncounterGeneratorActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import java.util.Queue
+import java.util.LinkedList
 
 // TODO: Add Characters to the map
 
@@ -13,8 +15,21 @@ class EncounterGeneratorActivity : AppCompatActivity() {
     // Gson() object used for serialization and deserialization with JSON strings
     val gson = Gson()
 
+    private var stream_growth = 20 // 20% chance an extra neighboring tile is also stream
+
     private var tiles_map = HashMap<Pair<Int, Int>, Tile>()
     private val tiles: ArrayList<Tile> = ArrayList()
+    private var stream_queue: Queue<Tile> = LinkedList<Tile>()
+
+    // Returns the 4 blocks around the input tile.
+    // Return may contain nulls.
+    fun return_near_tiles(tile: Tile): List<Tile?>{
+        var up_tile: Tile? = tiles_map[Pair(tile.x, tile.y+1)]
+        var down_tile: Tile? = tiles_map[Pair(tile.x, tile.y-1)]
+        var left_tile: Tile? = tiles_map[Pair(tile.x-1, tile.y)]
+        var right_tile: Tile? = tiles_map[Pair(tile.x+1, tile.y)]
+        return(listOf<Tile?>(up_tile, down_tile, left_tile, right_tile))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -94,6 +109,9 @@ class EncounterGeneratorActivity : AppCompatActivity() {
                 tiles.add(mytile)
                 tiles_map[Pair(i % width, i / height)] = mytile
             }
+            // Stream
+//            val list =
+
         }
         if (campfire == 1){
             // Choose a random square in the "center" of the board to place campfire
@@ -101,15 +119,23 @@ class EncounterGeneratorActivity : AppCompatActivity() {
             var rand_y = (((height - 1) / 4)..((height - 1) * 3 / 4)).random()
             // Find tile with said coordinates
             Log.d("CAMP", "rand_x and rand_y: " + rand_x + rand_y + "the rest expression " + height/4 + height * 3 / 4 + 7/2)
-            for (tile in tiles){
-                if (tile.x == rand_x && tile.y == rand_y){
-                    tile.content = Ground("campfire")
-                }
-            }
+//            for (tile in tiles){
+//                if (tile.x == rand_x && tile.y == rand_y){
+//                    tile.content = Ground("campfire")
+//                }
+//            }
+            tiles_map[Pair(rand_x, rand_y)]!!.content = Ground("campfire")
         }
 
         if (stream == 1){
-
+            var fountain_tile: Tile = tiles_map[Pair(2,2)] ?: throw Exception("Foundain tile is null")
+            for (tile in return_near_tiles(fountain_tile)){
+                stream_queue.add(tile)
+            }
+            for (tile in stream_queue){
+                // One of my neighbors becomes water block
+                return_near_tiles(tile).shuffled().take(1)[0]!!.content = Ground("water")
+            }
         }
     }
 }
