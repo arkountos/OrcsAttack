0dc833167b295d9911b8eca6989d865e13b9a28d
Fivos Iliadis <eternallistener@gmail.com>
2020-05-14 20:38:55 +0300

    Settings activity and layout

---
+++ app/src/main/java/com/example/inittrack2/ui/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inittrack2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.inittrack2.R.layout.activity_settings)
+    }
+}
