62945c188326cd19c627c1b5948d3ab7284ce644
Fivos Iliadis <eternallistener@gmail.com>
2020-05-15 23:27:42 +0300

    About activity

--- app/src/main/java/com/example/inittrack2/AboutActivity.kt
+++ app/src/main/java/com/example/inittrack2/AboutActivity.kt
@@ -1,12 +1,25 @@
 package com.example.inittrack2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        var feedback_button: Button = findViewById(R.id.feedback_button)
+        feedback_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "plain/text"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("fivosiliadis@gmail.com"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Orcs Attack!: ")
+            intent.putExtra(Intent.EXTRA_TEXT, "")
+            startActivity(Intent.createChooser(intent, ""))
+        }
     }
 }
