f6543e85c54790d867f1f8f31847910922c2b2de
Fivos Iliadis <eternallistener@gmail.com>
2020-05-20 16:02:35 +0300

    Syncc

--- app/src/main/java/com/example/inittrack2/AboutActivity.kt
+++ app/src/main/java/com/example/inittrack2/AboutActivity.kt
@@ -6,7 +6,9 @@ import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.text.method.LinkMovementMethod
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -15,6 +17,11 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        val about_text: TextView = findViewById(R.id.about_paragraph)
+        about_text.movementMethod = LinkMovementMethod.getInstance()
+
+        val about_attributions: TextView = findViewById(R.id.about_attribution)
+        about_attributions.movementMethod = LinkMovementMethod.getInstance()
 
         var feedback_button: Button = findViewById(R.id.feedback_button)
         feedback_button.setOnClickListener {
