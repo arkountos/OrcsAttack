d8984bd9e3b0c4ef99fc2e75cc753bf0ac029c87
arkountos <eternallistener@gmail.com>
2020-08-21 17:23:40 +0300

    Added a bunch of fonts

--- app/src/main/java/com/arkountos/orcsattack/NPCStatblockActivity.kt
+++ app/src/main/java/com/arkountos/orcsattack/NPCStatblockActivity.kt
@@ -1,17 +1,19 @@
 package com.arkountos.orcsattack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.npc_pretty.*
 
 class NPCStatblockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.npc_pretty)
-
+        var rootView = findViewById<ConstraintLayout>(R.id.statblock_root_view)
         var name = findViewById<TextView>(R.id.statblock_name)
-//        var alignment = findViewById<TextView>(R.id.statblock_alignment)
-
         name.text = "Fivos"
+
     }
 }
