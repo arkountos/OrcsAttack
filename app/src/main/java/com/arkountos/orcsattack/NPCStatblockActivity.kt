f5dd782ba24bd4cc697e3b3efe17c5d48c57c688
arkountos <eternallistener@gmail.com>
2020-08-22 17:20:32 +0300

    Changes in design and communication between NPCGenerator and NPCStatblock

--- app/src/main/java/com/arkountos/orcsattack/NPCStatblockActivity.kt
+++ app/src/main/java/com/arkountos/orcsattack/NPCStatblockActivity.kt
@@ -1,19 +1,34 @@
 package com.arkountos.orcsattack
 
+import android.graphics.Typeface
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import kotlinx.android.synthetic.main.npc_pretty.*
+import androidx.core.content.res.ResourcesCompat
 
 class NPCStatblockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.npc_pretty)
         var rootView = findViewById<ConstraintLayout>(R.id.statblock_root_view)
+        var nodestocapscondensed: Typeface? = ResourcesCompat.getFont(this, R.font.nodestocapscondensed)
+        var bookinsanityitalic: Typeface? = ResourcesCompat.getFont(this, R.font.bookinsanityitalic)
+
         var name = findViewById<TextView>(R.id.statblock_name)
-        name.text = "Fivos"
+        var alignment = findViewById<TextView>(R.id.statblock_alignment)
+
+        name.text = "Ghoul"
+        name.textSize = 50F
+        name.typeface = nodestocapscondensed
+        name.setTextColor(resources.getColor(R.color.colorStatblockRed))
+
+        alignment.text = "Medium undead, chaotic evil"
+        alignment.typeface = bookinsanityitalic
+        alignment.setTextColor(resources.getColor(R.color.colorBlack))
+
+
+
 
     }
 }
