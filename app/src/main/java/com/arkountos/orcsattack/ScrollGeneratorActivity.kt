f3b2ecdf2d524996a17bda4df51c28cf8262969e
arkountos <eternallistener@gmail.com>
2020-10-14 19:31:21 +0300

    Seekbar and parchment activity created

--- app/src/main/java/com/arkountos/orcsattack/ScrollGeneratorActivity.kt
+++ app/src/main/java/com/arkountos/orcsattack/ScrollGeneratorActivity.kt
@@ -2,7 +2,9 @@ package com.arkountos.orcsattack
 
 import android.content.Intent
 import android.graphics.Typeface
+import android.hardware.SensorEventListener
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +16,7 @@ class ScrollGeneratorActivity : AppCompatActivity() {
 
     private lateinit var font_option: Spinner
     private lateinit var font_result: Typeface
+    private lateinit var font_result_name: String
     private lateinit var font_size_option: Spinner
     private var font_size_result by Delegates.notNull<Int>()
 
@@ -21,26 +24,41 @@ class ScrollGeneratorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parchment_generator)
 
-        var lorem_ipsum = findViewById<TextView>(R.id.lorem_ipsum)
+        var lorem_ipsum = findViewById<EditText>(R.id.lorem_ipsum)
 
         var blackchancery: Typeface? = ResourcesCompat.getFont(this, R.font.blackchancery)
         var bookinsanity: Typeface? = ResourcesCompat.getFont(this, R.font.bookinsanity)
-//        var mreavessmallcaps: Typeface? = ResourcesCompat.getFont(this, R.font.mreavessmallcaps)
         var nodestocapscondensed: Typeface? = ResourcesCompat.getFont(this, R.font.nodestocapscondensed)
         var scalysans: Typeface? = ResourcesCompat.getFont(this, R.font.scalysans)
         var solberaimitation: Typeface? = ResourcesCompat.getFont(this, R.font.solberaimitation)
         var zatannamisdirection: Typeface? = ResourcesCompat.getFont(this, R.font.zatannamisdirection)
 
+        var fontnametouri: Map<String, Typeface> = mapOf("blackchancery" to blackchancery!!)
         //Add them all to an array and pick from the array inside the spinner
 
-        var fonts = arrayOf(blackchancery, bookinsanity, nodestocapscondensed, scalysans, solberaimitation, zatannamisdirection)
-        var fonts_names = arrayOf("Black Chancery", "Book Insanity", "Nodesto Caps Condensed", "Scaly Sans", "Solbera Imitation", "Zatanna Misdirection")
+        var fonts = arrayOf(
+            blackchancery,
+            bookinsanity,
+            nodestocapscondensed,
+            scalysans,
+            solberaimitation,
+            zatannamisdirection
+        )
+        var fonts_names = arrayOf(
+            "Black Chancery",
+            "Book Insanity",
+            "Nodesto Caps Condensed",
+            "Scaly Sans",
+            "Solbera Imitation",
+            "Zatanna Misdirection"
+        )
 
         font_option = findViewById(R.id.font_spinner)
         font_option.adapter = ArrayAdapter<String>(this, R.layout.spinner_item, fonts_names)
         font_option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 font_result = blackchancery!!
+                font_result_name = fonts_names[0]
             }
 
             override fun onItemSelected(
@@ -49,6 +67,7 @@ class ScrollGeneratorActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
+                font_result_name = fonts_names[position]
                 font_result = fonts[position]!!
                 lorem_ipsum.typeface = font_result
             }
@@ -56,28 +75,46 @@ class ScrollGeneratorActivity : AppCompatActivity() {
 
         var sizes_array = arrayOf(10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30)
 
-        font_size_option = findViewById(R.id.font_size_spinner)
-        font_size_option.adapter = ArrayAdapter<Int>(this, R.layout.spinner_item, sizes_array)
-        font_size_option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                font_size_result = 20
-            }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                font_size_result = sizes_array[position]
+        var seekBar = findViewById<SeekBar>(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                font_size_result = progress
                 lorem_ipsum.textSize = font_size_result.toFloat()
             }
-        }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+//        font_size_option = findViewById(R.id.font_size_spinner)
+//        font_size_option.adapter = ArrayAdapter<Int>(this, R.layout.spinner_item, sizes_array)
+//        font_size_option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                font_size_result = 20
+//            }
+//
+//            override fun onItemSelected(
+//                parent: AdapterView<*>?,
+//                view: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                font_size_result = sizes_array[position]
+//                lorem_ipsum.textSize = font_size_result.toFloat()
+//            }
+//        }
 
         var gotoParchmentButton = findViewById<Button>(R.id.gotoParchmentButton)
         gotoParchmentButton.setOnClickListener {
+            Log.d("con-in-tents", "font_result_name: " + font_result_name +", font_size_result: " + font_size_result.toString() + ", text: " + lorem_ipsum.text.toString())
             var intent = Intent(this, ScrollPresentationActivity::class.java)
-
+            intent.putExtra("EXTRA_FONT", font_result_name)
+            intent.putExtra("EXTRA_SIZE", font_size_result.toString())
+            intent.putExtra("EXTRA_TEXT", lorem_ipsum.text.toString())
             startActivity(intent)
         }
     }
--- app/src/main/java/com/arkountos/orcsattack/ScrollPresentationActivity.kt
+++ app/src/main/java/com/arkountos/orcsattack/ScrollPresentationActivity.kt
@@ -1,11 +1,41 @@
 package com.arkountos.orcsattack
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class ScrollPresentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_presentation)
+
+        var blackchancery: Typeface? = ResourcesCompat.getFont(this, R.font.blackchancery)
+        var bookinsanity: Typeface? = ResourcesCompat.getFont(this, R.font.bookinsanity)
+        var nodestocapscondensed: Typeface? = ResourcesCompat.getFont(this, R.font.nodestocapscondensed)
+        var scalysans: Typeface? = ResourcesCompat.getFont(this, R.font.scalysans)
+        var solberaimitation: Typeface? = ResourcesCompat.getFont(this, R.font.solberaimitation)
+        var zatannamisdirection: Typeface? = ResourcesCompat.getFont(this, R.font.zatannamisdirection)
+
+        var fontnametouri: Map<String, Typeface> = mapOf(
+            "Black Chancery" to blackchancery!!,
+            "Book Insanity" to bookinsanity!!,
+            "Nodesto Caps Condensed" to nodestocapscondensed!!,
+            "Scaly Sans" to scalysans!!,
+            "Solbera Imitation" to solberaimitation!!,
+            "Zatanna Misdirection" to zatannamisdirection!!)
+
+        var font_result = intent.getStringExtra("EXTRA_FONT")
+        var font_size_result = intent.getStringExtra("EXTRA_SIZE")
+        var text_result = intent.getStringExtra("EXTRA_TEXT")
+
+        Log.d("Target activity:", "$font_result  $font_size_result  $text_result")
+
+        var note = findViewById<TextView>(R.id.note)
+        note.text = text_result.toString()
+        note.textSize = font_size_result.toFloat()
+        note.typeface = fontnametouri[font_result]
     }
 }
