c3cc6d321740b1749cb0492f89065005408422e3
arkountos <eternallistener@gmail.com>
2020-10-15 17:20:37 +0300

    Export functionality and version codes update

--- app/src/main/java/com/arkountos/orcsattack/ScrollPresentationActivity.kt
+++ app/src/main/java/com/arkountos/orcsattack/ScrollPresentationActivity.kt
@@ -1,13 +1,32 @@
 package com.arkountos.orcsattack
 
+import android.Manifest
+import android.app.Dialog
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.npc_pretty.*
+import java.io.File
+import java.io.FileOutputStream
 
 class ScrollPresentationActivity : AppCompatActivity() {
+
+    private var STORAGE_PERMISSION_CODE = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_presentation)
@@ -37,5 +56,99 @@ class ScrollPresentationActivity : AppCompatActivity() {
         note.text = text_result.toString()
         note.textSize = font_size_result.toFloat()
         note.typeface = fontnametouri[font_result]
+
+        var export_button = findViewById<Button>(R.id.scroll_presentation_export)
+        export_button.setOnClickListener {
+            Log.d("checkPermission:", "before")
+            checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
+            Log.d("checkPermission:", "after")
+
+            var fileName: String = ""
+
+            val alertDialog = Dialog(this)
+            var inflater = LayoutInflater.from(this)
+            val view: View = inflater.inflate(R.layout.npc_save_path_dialog, null)
+            val pathEdittext = view.findViewById<EditText>(R.id.npc_save_path_dialog_edittext)
+            val okButton = view.findViewById<Button>(R.id.npc_save_path_dialog_ok_button)
+            okButton.setOnClickListener {
+                Log.d("Btn", "Clicked!" + "")
+                fileName = pathEdittext.text.toString()
+                // generate the image path
+                val imagePath: String = Environment.getExternalStorageDirectory()
+                    .toString() + File.separator.toString() + fileName + ".png"
+
+                try {
+
+                    // save the image as png
+                    val out = FileOutputStream(imagePath)
+                    // compress the image to png and pass it to the output stream
+                    loadBitmapFromView(statblock_root_view)!!.compress(
+                        Bitmap.CompressFormat.PNG,
+                        90,
+                        out
+                    )
+
+                    // save the image
+                    out.flush()
+                    out.close()
+
+                    Toast.makeText(this, "Saved as $imagePath", Toast.LENGTH_LONG).show()
+                } catch (error: Exception) {
+                    Log.e("Error saving image", error.message)
+                }
+                alertDialog.dismiss()
+            }
+            val cancelButton = view.findViewById<Button>(R.id.npc_save_path_dialog_cancel_button)
+            cancelButton.setOnClickListener {
+                Log.d("Btn", "Clicked Cancel!")
+                alertDialog.dismiss()
+            }
+            alertDialog.setContentView(view)
+            alertDialog.show()
+        }
     }
+
+    fun loadBitmapFromView(view: View): Bitmap? {
+
+        // width measure spec
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(
+            view.measuredWidth, View.MeasureSpec.EXACTLY
+        )
+        // height measure spec
+        val heightSpec = View.MeasureSpec.makeMeasureSpec(
+            view.measuredHeight, View.MeasureSpec.EXACTLY
+        )
+        // measure the view
+        view.measure(widthSpec, heightSpec)
+        // set the layout sizes
+        view.layout(
+            view.left,
+            view.top,
+            view.measuredWidth + view.left,
+            view.measuredHeight + view.top
+        )
+        // create the bitmap
+        val bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        // create a canvas used to get the view's image and draw it on the bitmap
+        val c = Canvas(bitmap)
+        // position the image inside the canvas
+        c.translate((-view.scrollX).toFloat(), (-view.scrollY).toFloat())
+        // get the canvas
+        view.draw(c)
+        return bitmap
+    }
+
+
+    // Function to check and request permission
+    private fun checkPermission(permission: String, requestCode: Int) {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(this,permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission),requestCode)
+        }
+        else {
+            Toast.makeText(this,"Permission already granted",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
