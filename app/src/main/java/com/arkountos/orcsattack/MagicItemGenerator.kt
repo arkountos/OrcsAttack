cf8d0c9e21286228772b21cbc31b418f43ce6c05
arkountos <eternallistener@gmail.com>
2020-10-21 21:21:16 +0300

    Requests to Open5e return magic items randomly

--- app/src/main/java/com/arkountos/orcsattack/MagicItemGenerator.kt
+++ app/src/main/java/com/arkountos/orcsattack/MagicItemGenerator.kt
@@ -3,8 +3,10 @@ package com.arkountos.orcsattack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
@@ -13,19 +15,35 @@ class MagicItemGenerator : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_magic_item_generator)
 
-        var result = ""
+        Log.d("Magic", ":O")
+        var textview = findViewById<TextView>(R.id.response)
+
+//        var result = ""
 
         val queue = Volley.newRequestQueue(this)
-        val url = "https://api.open5e.com/magicitems/"
 
-        val stringRequest = StringRequest(Request.Method.GET, url, { response ->
-            result += response.substring(0,500)
+        val randomMagicItemNumber = (0..238).random()
+        val randomMagicItemPage = randomMagicItemNumber / 50
+        val randomMagicItemIndex = randomMagicItemNumber % 50
+
+        var url = "https://api.open5e.com/magicitems/"
+        when (randomMagicItemPage){
+            1 -> url += ""
+            2 -> url += "?page=2"
+            3 -> url += "?page=3"
+            4 -> url += "?page=4"
+            5 -> url += "?page=5"
+        }
+//        Log.d("Item nums", "$randomMagicItemNumber, $randomMagicItemPage, $randomMagicItemIndex")
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+            textview.text = response.getJSONArray("results").get(randomMagicItemIndex).toString()
         },
             {
-                result += "Nope!"
+                textview.text = "Nope!"
             })
 
-        queue.add(stringRequest)
-        Log.d("Reply", result)
+        queue.add(jsonObjectRequest)
     }
 }
